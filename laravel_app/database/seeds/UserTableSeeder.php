21f46cf1e3dc64d496b542664dec80dcde3e9343
miwa <dsnap1993@gmail.com>
2019-02-24 19:08:12 +0900

    first commit

---
+++ laravel_app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => str_random(10),
+                'email' => str_random(10).'@gmail.com',
+                'password' => bcrypt('secret'),
+            ]);
+        }
+    }
+}
