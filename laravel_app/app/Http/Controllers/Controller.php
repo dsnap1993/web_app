439129e1d78fd2f5f1d44de977805c8c6ce86bd3
dsnap1993 <dsnap1993@gmail.com>
2019-02-26 16:22:56 +0900

    add a method

--- laravel_app/app/Http/Controllers/Controller.php
+++ laravel_app/app/Http/Controllers/Controller.php
@@ -6,8 +6,23 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Log;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * encode request object to json
+     * @param       Request $request
+     * @return      json
+     */
+    private function encodeToJson(Request $request)
+    {
+        $arrayRequest = $request->all();
+        Log::debug(__METHOD__ . 'arrayRequest = ' . $arrayRequest);
+        $jsonRequest = json_encode($arrayRequest);
+        Log::debug(__METHOD__ . 'jsonRequest = ' . $jsonRequest);
+        return $jsonRequest;
+    }
 }
