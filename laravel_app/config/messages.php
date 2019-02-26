bf8aa9f607cab060c0bf60eac7aa60c9514e4e8d
miwa <dsnap1993@gmail.com>
2019-02-26 22:35:43 +0900

    first commit

---
+++ laravel_app/config/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'error' => [
+        'login' => [
+            'fail' => 'Wrong email or password. Try again or click Forgot password to reset it.',
+            'locked' => 'Your account is being locked.',
+            'other' => 'It seems an error occured. Try again later. If the error is not resolved, contact to web administrator.',
+        ],
+    ],
+];
