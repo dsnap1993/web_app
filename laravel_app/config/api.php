fb5dba7b653be4fbdb61d29385be58e7fdb1009d
miwa <dsnap1993@gmail.com>
2019-03-03 13:35:29 +0900

    first commit

---
+++ laravel_app/config/api.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    /* API URL */
+    'url' => env('API_URL'),
+
+    /* API TIMEOUT */
+    'timeout' => env('API_TIMEOUT'),
+    
+    /* users */
+    'users' => '/users',
+);
