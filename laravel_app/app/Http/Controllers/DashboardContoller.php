91d702a496221b76774b98d238a870bd5e8d1c64
miwa <dsnap1993@gmail.com>
2019-03-03 15:13:01 +0900

    first commit

---
+++ laravel_app/app/Http/Controllers/DashboardContoller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\WebAPI\WebAPI;
+use Log;
+
+class DashboardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function show()
+    {
+        return view('dashboard.index');
+    }
+}
