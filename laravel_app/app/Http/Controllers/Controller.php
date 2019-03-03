e78033b643f332f1089fd578683400c214aa63af
miwa <dsnap1993@gmail.com>
2019-03-03 15:12:39 +0900

    modified

--- laravel_app/app/Http/Controllers/Controller.php
+++ laravel_app/app/Http/Controllers/Controller.php
@@ -2,29 +2,15 @@
 
 namespace App\Http\Controllers;
 
-//use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
-//use Illuminate\Foundation\Validation\ValidatesRequests;
-//use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Log;
 
 class Controller extends BaseController
 {
-    //use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * encode request object to json
-     * @param       Request $request
-     * @return      json
-     */
-    protected function encodeToJson(Request $request)
-    {
-        $arrayRequest = $request->all();
-        unset($arrayRequest['_token']);
-        $jsonRequest = json_encode($arrayRequest);
-        Log::debug(__METHOD__ . ' jsonRequest = ' . $jsonRequest);
-        return $jsonRequest;
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
--- laravel_app/routes/web.php
+++ laravel_app/routes/web.php
@@ -10,9 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', IndexAction::class)->name('index');
-Route::get('/login', 'LoginAction@show')->name('login');
-Route::post('/login/post_login', 'LoginAction@postLogin')->name('login');
-//Route::get('/dashboard', DashboardAction::class)->name('dashboard');
-//Route::get('/create_user', CreateUserAction::class)->name('create_user');
-//Route::get('/capture_packet', CapturePacketAction::class)->name('capture_packet');
+
+/* login page */
+Route::get('/', IndexController::class)->name('index');
+Route::get('/login', 'LoginController@show')->name('login');
+Route::post('/login/post_login', 'LoginController@postLogin')->name('login');
+
+Auth::routes();
+
+/* dashboard page */
+Route::get('/dashboard', 'DashboardController@show')->name('dashboard');
+//Route::get('/create_user', CreateUserController::class)->name('create_user');
+//Route::get('/capture_packet', CapturePacketController::class)->name('capture_packet');
