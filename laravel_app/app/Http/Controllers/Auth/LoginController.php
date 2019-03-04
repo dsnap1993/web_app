93156336eb0d6d39e7e560f4d980891f963c6f14
miwa <dsnap1993@gmail.com>
2019-03-04 21:42:38 +0900

    undid all of class

--- laravel_app/app/Http/Controllers/Auth/LoginController.php
+++ laravel_app/app/Http/Controllers/Auth/LoginController.php
@@ -5,9 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use App\Http\Requests;
-use App\Http\WebAPI\WebAPI;
-use Log;
 
 class LoginController extends Controller
 {
@@ -38,44 +35,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-
-    /**
-     * Show login page.
-     * 
-     * @return  \Illuminate\Contracts\Support\Renderable
-     */
-    public function show()
-    {
-        return view('auth.login');
-    }
-
-    /**
-     * Execute Logging in.
-     * @params  Request $request
-     * @return  \Illuminate\Contracts\Support\Renderable
-     */
-    public function postLogin(Request $request)
-    {
-        $apiPath = config('api.users');
-        $requestParams = $request->all();
-        unset($requestParams['_token']);
-
-        $webApi = new WebAPI;
-        $response = $webApi->callAPI($requestParams, 'GET', $apiPath);
-        Log::debug(__METHOD__ . ' response = ' . print_r($response, true));
-
-        if ($response['statusCode'] == 200) {
-            return redirect()->to('/dashboard');
-        } else if ($response['statusCode'] == 401) {
-            $errMsg = config('messages.error.login.fail');
-            return redirect()->back()->withErrors($errMsg);
-        } else if ($response['statusCode'] == 403) {
-            $errMsg = config('messages.error.login.locked');
-            return redirect()->back()->withErrors($errMsg);
-        } else {
-            $errMsg = config('messages.error.login.other');
-            return redirect()->back()->withErrors($errMsg);
-        }
     }
 }
