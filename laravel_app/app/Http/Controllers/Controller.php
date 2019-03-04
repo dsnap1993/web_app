a02c1e7c03083d950c5f7d69557f81e106ada084
miwa <dsnap1993@gmail.com>
2019-03-04 21:41:25 +0900

    added checkLoggingIn and formatSessionData

--- laravel_app/app/Http/Controllers/Controller.php
+++ laravel_app/app/Http/Controllers/Controller.php
@@ -13,4 +13,39 @@ use Log;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Check whether logging in
+     * 
+     * @param   Request  $request
+     * @param   $path   a path of redirected page
+     * @return  boolean
+     */
+    protected function checkLoggingIn($request)
+    {
+        $sessionData = $request->session()->all();
+        Log::debug(__METHOD__ . ' sessionData = ' . print_r($sessionData, true));
+        $result = $this->formatSessionData($sessionData);
+        Log::debug(__METHOD__ . ' result = ' . print_r($result, true));
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Format array of session data
+     * 
+     * @param   array   $sessionData
+     * @return  array
+     */
+    private function formatSessionData($sessionData)
+    {
+        unset($sessionData['_token']);
+        unset($sessionData['_previous']);
+        unset($sessionData['_flash']);
+
+        return $sessionData;
+    }
 }
