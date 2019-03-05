40ba1cdccadb18871684a9e076ed7d5eef69263f
dsnap1993 <dsnap1993@gmail.com>
2019-03-05 15:10:16 +0900

    modified name of class

--- laravel_app/app/Http/Controllers/LogoutController.php
+++ laravel_app/app/Http/Controllers/LogoutController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
-class LoginController extends Controller
+class LogoutController extends Controller
 {
     /**
      * Create a new controller instance.
