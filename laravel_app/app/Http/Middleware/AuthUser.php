91aee86ec4f5b76e67d68a023d5dcaaf00ed10e4
dsnap1993 <dsnap1993@gmail.com>
2019-03-05 15:02:02 +0900

    first commit

---
+++ laravel_app/app/Http/Middleware/AuthUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+//use Illuminate\Http\Request;
+//use App\Http\Requests;
+use App\Http\WebAPI\WebAPI;
+use Closure;
+use Log;
+
+class AuthUser
+{
+    public function handle($request, Closure $next)
+    {
+        // check whether setting data on session
+        if (!($request->session()->has('user_id'))) {
+            $request->session()->flash('msg_session_timeout', 'Session Time Out');
+            return redirect()->to('/');
+        }
+
+        // call API GET /users
+        $webApi = new WebAPI;
+        $apiPath = config('api.users');
+
+        $requestParams = array(
+            'email' => $request->session()->get('email'),
+            'password' => $request->session()->get('password'),
+        );
+        $response = $webApi->callAPI($requestParams, 'GET', $apiPath);
+
+        // redirect page
+        if ($response['statusCode'] == 200) {
+            return $next($request);
+        } else {
+            $request->session()->flash('msg_session_timeout', 'Session Time Out');
+            return redirect()->to('/');
+        }
+    }
+}
