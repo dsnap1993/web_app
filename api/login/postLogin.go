a583a9313d7d78b8c303bba6b26246fbbfaf489b
miwa <dsnap1993@gmail.com>
2019-03-06 21:19:38 +0900

    modified name of struct

--- api/login/postLogin.go
+++ api/login/postLogin.go
@@ -13,18 +13,18 @@ import (
 	//"../env"
 )
 
-type requestForGET struct {
+type request struct {
 	Email 		string 	`json:"email"`
 	Password 	string 	`json:"password"`
 }
 
-type responseForGET struct {
+type response struct {
 	UserId 		int		`json:"user_id"`
 	Email 		string 	`json:"email"`
 	Name		string  `json:"name"`
 }
 
-func (request *requestForGET) validate() (bool, string) {
+func (request *request) validate() (bool, string) {
 	errMsg := ""
 	result := true
 	if (*request).Email == "" || (*request).Password == ""{
@@ -35,33 +35,33 @@ func (request *requestForGET) validate() (bool, string) {
 }
 
 func PostLogin(c echo.Context) error {
-	request := new(requestForGET)
+	request := new(request)
 	if err := c.Bind(request); err != nil {
-		log.Printf("users/GetUser: %s", err)
+		log.Printf("login/GetUser: %s", err)
 		os.Exit(1)
 	}
 
 	result, errMsg := request.validate()
 	if result == false {
-		log.Printf("[response] %d %s", status, responseData)
+		log.Printf("[response] %d %s", http.StatusBadRequest, errMsg)
 		return c.JSON(http.StatusBadRequest, errMsg)
 	}
 
 	data, status := selectData(request)
-	status, responseData := createResponseForGetUser(data, status)
+	status, responseData := createResponse(data, status)
 
 	if status == http.StatusOK {
 		log.Printf("[response] %d %s", status, responseData)
 		return c.JSON(status, responseData)
 	} else {
-		log.Printf("[response] %d %s", status, responseData)
+		log.Printf("[response] %d", status)
 		return c.JSON(status, nil)
 	}
 }
 
-func createResponseForGetUser(data *db.UsersTable, status int) (int, *responseForGET) {
+func createResponse(data *db.UsersTable, status int) (int, *response) {
 	if status == http.StatusOK {
-		responseData := &responseForGET{
+		responseData := &response{
 			UserId: (*data).UserId,
 			Email: (*data).Email,
 			Name: (*data).Name,
@@ -72,18 +72,18 @@ func createResponseForGetUser(data *db.UsersTable, status int) (int, *responseFo
 	}
 }
 
-func selectData(request *requestForGET) (*db.UsersTable, int) {
+func selectData(request *request) (*db.UsersTable, int) {
 	//env.LoadEnv()
 	dbConn, dbErr := db.ConnectDB()
 	if dbErr != nil {
-		log.Printf("users/selectData: dbErr = %s", dbErr)
+		log.Printf("login/selectData: dbErr = %s", dbErr)
 		os.Exit(1)
 	}
 	defer dbConn.Close()
 
 	data, err := dbConn.Query("SELECT * FROM users WHERE email=?", (*request).Email)
 	if err != nil {
-		log.Printf("users/selectData: err = %s", err)
+		log.Printf("login/selectData: err = %s", err)
 		return nil, http.StatusInternalServerError
 	}
 
@@ -104,7 +104,7 @@ func selectData(request *requestForGET) (*db.UsersTable, int) {
 
 		err := data.Scan(&userId, &name, &email, &password, &createdAt, &updatedAt, &isLocked, &failureCount, &unlockedAt)
 		if err != nil {
-			log.Printf("users/selectUser: err = %s", err)
+			log.Printf("login/selectUser: err = %s", err)
 			os.Exit(1)
 		}
 		user.UserId = userId
@@ -125,7 +125,7 @@ func selectData(request *requestForGET) (*db.UsersTable, int) {
 	errComparingPasswd := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte((*request).Password))
 
 	if errComparingPasswd != nil {
-		log.Printf("users/selectUser: errComparingPasswd = %s", errComparingPasswd)
+		log.Printf("login/selectUser: errComparingPasswd = %s", errComparingPasswd)
 		if isLockedAccount(dbConn, request) {
 			return nil, http.StatusForbidden
 		}
@@ -152,12 +152,12 @@ func selectData(request *requestForGET) (*db.UsersTable, int) {
 	return &user, http.StatusOK
 }
 
-func increaseFailureCount(dbConn *sql.DB, request *requestForGET) int {
+func increaseFailureCount(dbConn *sql.DB, request *request) int {
 	var failureCount int
 
 	errSelecting := dbConn.QueryRow("SELECT failure_count FROM users WHERE email=?", (*request).Email).Scan(&failureCount)
 	if errSelecting != nil {
-		log.Printf("users/increaseFailureCount: errSelecting = %s", errSelecting)
+		log.Printf("login/increaseFailureCount: errSelecting = %s", errSelecting)
 		os.Exit(1)
 	}
 
@@ -169,28 +169,28 @@ func increaseFailureCount(dbConn *sql.DB, request *requestForGET) int {
 func updateUsers(dbConn *sql.DB, email string, failureCount int) {
 	_, err := dbConn.Query("UPDATE users SET failure_count=? WHERE email=?", failureCount, email)
 	if err != nil {
-		log.Printf("users/updateUsers: err = %s", err)
+		log.Printf("login/updateUsers: err = %s", err)
 		os.Exit(1)
 	}
 }
 
-func lockAccount(dbConn *sql.DB, request *requestForGET) {
+func lockAccount(dbConn *sql.DB, request *request) {
 	now := time.Now()
 	unlockedAt := now.Add(24 * time.Hour)
 	formatedTime := unlockedAt.Format("2006-01-02 15:04:05")
 
 	_, err := dbConn.Query("UPDATE users SET is_locked=1, failure_count=0,unlocked_at=? WHERE email=?", formatedTime, (*request).Email)
 	if err != nil {
-		log.Printf("users/lockAccount: err = %s", err)
+		log.Printf("login/lockAccount: err = %s", err)
 		os.Exit(1)
 	}
 }
 
-func isLockedAccount(dbConn *sql.DB, request *requestForGET) bool {
+func isLockedAccount(dbConn *sql.DB, request *request) bool {
 	var isLocked bool
 	err := dbConn.QueryRow("SELECT is_locked FROM users WHERE email=?", (*request).Email).Scan(&isLocked)
 	if err != nil {
-		log.Printf("users/isLockedAccount: err = %s", err)
+		log.Printf("login/isLockedAccount: err = %s", err)
 		os.Exit(1)
 	}
 	return isLocked
@@ -199,7 +199,7 @@ func isLockedAccount(dbConn *sql.DB, request *requestForGET) bool {
 func unlockAccount(dbConn *sql.DB, email string) {
 	_, err := dbConn.Query("UPDATE users SET is_locked=0, unlocked_at=0 WHERE email=?", email)
 	if err != nil {
-		log.Printf("users/unlockAccount: err = %s", err)
+		log.Printf("login/unlockAccount: err = %s", err)
 		os.Exit(1)
 	}
 }
@@ -210,7 +210,7 @@ func isPastedLockingPeriod(dbConn *sql.DB, email string) bool {
 
 	err := dbConn.QueryRow("SELECT unlocked_at FROM users WHERE email=?", email).Scan(&unlockedAt)
 	if err != nil {
-		log.Printf("users/isPastedLockingAccount: err = %s", err)
+		log.Printf("login/isPastedLockingAccount: err = %s", err)
 		os.Exit(1)
 	}
 	if strToTime(unlockedAt).Before(now) {
@@ -223,7 +223,7 @@ func isPastedLockingPeriod(dbConn *sql.DB, email string) bool {
 func strToTime(str string) time.Time {
 	t, err := time.Parse("2006-01-02 15:04:05", str)
 	if err != nil {
-		log.Printf("users/strToTime: err = %s", err)
+		log.Printf("login/strToTime: err = %s", err)
 		os.Exit(1)
 	}
 	return t
