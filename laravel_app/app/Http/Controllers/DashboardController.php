83942a0e0671c22012538ddb27397ca5f6b46d7d
miwa <dsnap1993@gmail.com>
2019-03-07 22:34:35 +0900

    added some processes

--- laravel_app/app/Http/Controllers/DashboardController.php
+++ laravel_app/app/Http/Controllers/DashboardController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\WebAPI\WebAPI;
 use Log;
@@ -22,11 +21,31 @@ class DashboardController extends Controller
     /**
      * Show dashboard index page
      * 
-     * @param   Request     $request
+     * @param   DashboardRequest     $request
      * @return  \Illuminate\Contracts\Support\Renderable
      */
-    public function index(Request $request)
+    public function index(DashboardRequest $request)
     {
-            return view('dashboards.index');
+        $apiPath = config('api.capture_data');
+        $requestParams = array(
+            'user_id' => $request->session()->get('user_id'),
+        );
+
+        // call API GET /capture_data.jsom
+        $webApi = new WebAPI;
+        $response = $webApi->callAPI($requestParams, 'GET', $apiPath);
+        Log::debug(__METHOD__ . ' response = ' . print_r($response, true));
+
+        // set response data in session
+        $array = json_decode($response['body'], true);
+        Log::debug(__METHOD__ . ' array = ' . print_r($array, true));
+
+        if ($response['statusCode'] === 200) {
+            return view('dashboards.index', compact('array'));
+        } else {
+            $errMsg = config('messages.error.dashboard.fail');
+            $request->session()->flash('message', $errMsg);
+            return view('dashboards.index', $errMsg);
+        }
     }
 }
