f38d8090f6ecae97ae46f014d340372ad4794c8e
miwa <dsnap1993@gmail.com>
2019-03-09 21:59:00 +0900

    added the name of route login

--- laravel_app/routes/web.php
+++ laravel_app/routes/web.php
@@ -12,16 +12,18 @@
 */
 
 /* login page */
-Route::get('/', IndexController::class)->name('index');
+Route::get('/', IndexController::class);
 Route::get('/login', 'LoginController@show')->name('login');
-Route::post('/login', 'LoginController@postLogin')->name('login');
+Route::post('/login', 'LoginController@postLogin');
 
 /* logout */
 Route::get('/logout', LogoutController::class)->name('logout');
 
 Route::middleware(['auth_user'])->group(function(){
     /* dashboard page */
-    Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    Route::get('/dashboard', 'DashboardController@index');
+    Route::put('/dashboard', 'DashboardController@update');
+    Route::delete('/dashboard', 'DashboardController@delete');
     //Route::get('/create_user', CreateUserController::class)->name('create_user');
     //Route::get('/capture_packet', CapturePacketController::class)->name('capture_packet');
 });
