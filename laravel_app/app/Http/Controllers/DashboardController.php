7fef0b43a491e563511a05520ef0cb34087fe15c
miwa <dsnap1993@gmail.com>
2019-03-09 23:07:25 +0900

    added the function update, delete

--- laravel_app/app/Http/Controllers/DashboardController.php
+++ laravel_app/app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests;
+use Illuminate\Http\Request;
 use App\Http\WebAPI\WebAPI;
 use Log;
 
@@ -24,16 +24,80 @@ class DashboardController extends Controller
      * @param   DashboardRequest     $request
      * @return  \Illuminate\Contracts\Support\Renderable
      */
-    public function index(DashboardRequest $request)
+    public function index(Request $request)
     {
-        $apiPath = config('api.capture_data');
+        $apiPath = config('api.ver') . config('api.capture_data');
         $requestParams = array(
             'user_id' => $request->session()->get('user_id'),
         );
 
-        // call API GET /capture_data.jsom
+        // call API GET /capture_data.json
         $webApi = new WebAPI;
         $response = $webApi->callAPI($requestParams, 'GET', $apiPath);
+        Log::debug(__METHOD__ . ' response = ' . print_r($response, true));
+
+        // set response data in session
+        $array = json_decode($response['body'], true);
+        Log::debug(__METHOD__ . ' array = ' . print_r($array, true));
+
+        if ($response['statusCode'] === 200) {
+            return view('dashboards.index', compact('array'));
+        } else {
+            $errMsg = config('messages.error.dashboard.fail');
+            $request->session()->flash('message', $errMsg);
+            return view('dashboards.index');
+        }
+    }
+
+    /**
+     * Update capture data on dashboard page
+     * 
+     * @param   Request    $request
+     * @return  \Illuminate\Contracts\Support\Renderable
+     */
+    public function update(Request $request)
+    {
+        $apiPath = config('api.capture_data');
+        $requestParams = array(
+            'data_id' => $request->input('data_id'),
+            'data_name' => $request->input('data_name'),
+            'data_summary' => $request->input('data_summary'),
+        );
+
+        // call API PUT /capture_data.json
+        $webApi = new WebAPI;
+        $response = $webApi->callAPI($requestParams, 'PUT', $apiPath);
+        Log::debug(__METHOD__ . ' response = ' . print_r($response, true));
+
+        // set response data in session
+        $array = json_decode($response['body'], true);
+        Log::debug(__METHOD__ . ' array = ' . print_r($array, true));
+
+        if ($response['statusCode'] === 200) {
+            return view('dashboards.index', compact('array'));
+        } else {
+            $errMsg = config('messages.error.dashboard.fail');
+            $request->session()->flash('message', $errMsg);
+            return view('dashboards.index', $errMsg);
+        }
+    }
+
+    /**
+     * Delete capture data on dashboard page
+     * 
+     * @param   Request    $request
+     * @return  \Illuminate\Contracts\Support\Renderable
+     */
+    public function delete(Request $request)
+    {
+        $apiPath = config('api.capture_data');
+        $requestParams = array(
+            'data_id' => $request->input('data_id'),
+        );
+
+        // call API DELETE /capture_data.json
+        $webApi = new WebAPI;
+        $response = $webApi->callAPI($requestParams, 'DELETE', $apiPath);
         Log::debug(__METHOD__ . ' response = ' . print_r($response, true));
 
         // set response data in session
