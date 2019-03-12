b58c0a7332c1ada26971949378af52a4bd6eb4bb
miwa <dsnap1993@gmail.com>
2019-03-12 21:35:33 +0900

    delete modal

--- laravel_app/resources/views/dashboards/index.blade.php
+++ laravel_app/resources/views/dashboards/index.blade.php
@@ -57,26 +57,5 @@
             </div>
         </div>
     </div>
-
-    <!-- Modal of Deleting -->
-    <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <div class="modal-header" id="deleteModalHeader">
-                <h5 class="modal-title" id="deleteModalLabel">Confirmation</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Cancel">
-                <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Are you sure you want to delete this data?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary">Delete</button>
-            </div>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
