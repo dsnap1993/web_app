520b1566d40a238a1e3c309fda9ba8c7a5a8918a
miwa <dsnap1993@gmail.com>
2019-03-15 23:43:47 +0900

    modified script

--- laravel_app/resources/views/layouts/app.blade.php
+++ laravel_app/resources/views/layouts/app.blade.php
@@ -10,7 +10,9 @@
     <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/modal.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
