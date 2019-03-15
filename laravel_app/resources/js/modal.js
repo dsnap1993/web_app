0b8c338b5ceba822223f197d23d2c8a6dce062ab
miwa <dsnap1993@gmail.com>
2019-03-15 23:43:34 +0900

    added events os updating and deleting modals

--- laravel_app/resources/js/modal.js
+++ laravel_app/resources/js/modal.js
@@ -1,25 +1,63 @@
-require('./bootstrap');
-require('./app');
+// modal of editing on dashboard
+$(document).ready(function() {
 
-$('#modal-edit').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget)
-    var data = button.data('whatever')
-    /**
-     * data[0] => data_id, data_id[1] => user_id, data[2] => data_name
-     * data[3] => data_summary, data[4] => created_at, data[5] => file_name
-     */
+    $(document).on('click', "#edit-button", function() {
+        $(this).addClass('edit-button-trigger-clicked');
+    
+        var options = {
+          'backdrop': 'static'
+        };
+        $('#modal-edit').modal(options)
+    })
 
-    var modal = $(this)
-    modal.find('#editModalLavel').text('Editing ' + data[2])
-    modal.find('.modal-body input#data-name').val(data[2])
-    modal.find('.modal-body input#data-summary').val(data[3])
-    modal.find('.modal-body input#file-name').val(data[5])
-})
+    $('#modal-edit').on('show.bs.modal', function () {
+        var button = $(".edit-button-trigger-clicked");
+        var row = button.closest(".data-row");
 
-$('#modal-delete').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget)
-    var dataId = button.data('whatever')
+        var dataId = button.data('id');
+        var dataName = row.children(".data-name").text();
+        var dataSummary = row.children(".data-summary").text();
+        var fileName = row.children(".file-name").text();
 
-    var modal = $(this)
-    modal.find('#deleteModalLavel').text('Confirmation ' + dataId)
-})
+        $("#modal-data-name").val(dataName);
+        $("#modal-data-summary").val(dataSummary);
+        $("#modal-file-name").val(fileName);
+        $("#edit-data-id").val(dataId);
+    });
+
+    $('#modal-edit').on('hide.bs.modal', function() {
+        $('.edit-button-trigger-clicked').removeClass('edit-button-trigger-clicked')
+        $("#edit-form").trigger("reset");
+    })
+
+    $('#modal-delete').on('show.bs.modal', function (event) {
+        var button = $("delete-button-trigger-clicked");
+        var dataId = button.data('id');
+
+    });
+});
+
+// modal of deleting on dashboard
+$(document).ready(function() {
+
+    $(document).on('click', "#delete-button", function() {
+        $(this).addClass('delete-button-trigger-clicked');
+    
+        var options = {
+          'backdrop': 'static'
+        };
+        $('#modal-delete').modal(options)
+    })
+
+    $('#modal-delete').on('show.bs.modal', function (event) {
+        var button = $(".delete-button-trigger-clicked");
+        var dataId = button.data('id');
+
+        $("#delete-data-id").val(dataId);
+    });
+
+    $('#modal-delete').on('hide.bs.modal', function() {
+        $('.edit-button-trigger-clicked').removeClass('delete-button-trigger-clicked')
+        $("#delete-form").trigger("reset");
+    })
+});
