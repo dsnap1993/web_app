d202303c15d73e03dcd7da1b37d16a6f7a1ef4ab
miwa <dsnap1993@gmail.com>
2019-03-27 22:24:40 +0900

    modified

--- laravel_app/resources/views/auth/passwords/email.blade.php
+++ laravel_app/resources/views/auth/passwords/email.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header" id="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('password.email') }}">
+                    <form method="POST" action="{{ route('send_email') }}">
                         @csrf
 
                         <div class="form-group row">
@@ -33,6 +33,9 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
+                                <button type="button" class="btn btn-secondary" onclick="history.back()">
+                                    {{ __('Back') }}
+                                </button>
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
